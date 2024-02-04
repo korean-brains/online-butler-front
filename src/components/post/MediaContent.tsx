b2e598d99aef3de7f50713a92e5d038199b0b4f1
skatn <87296704+skatn@users.noreply.github.com>
2024-02-04 20:56:36 +0900

    MediaContent 이미지 1:1사이즈 적용

--- src/components/post/MediaContent.tsx
+++ src/components/post/MediaContent.tsx
@@ -7,7 +7,11 @@ interface MediaContentProps {
 const MediaContent = ({ img, current, total }: MediaContentProps) => {
   return (
     <>
-      <img src={img} alt="content" />
+      <img
+        src={img}
+        alt="content"
+        className="aspect-square w-full object-cover"
+      />
       <span className="absolute right-3 top-2 rounded-full bg-black bg-opacity-50 px-3 py-1 text-xs text-white">
         {current}/{total}
       </span>
