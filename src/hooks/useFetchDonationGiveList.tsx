6c2588b676ef9512d7cf957a5ba6ec2ac5ee37fc
skatn <87296704+skatn@users.noreply.github.com>
2024-05-11 22:14:10 +0900

    #28 fix: 후원 내역 페이지 API 적용

---
+++ src/hooks/useFetchDonationGiveList.tsx
@@ -0,0 +1,44 @@
+import { useQuery } from 'react-query';
+import butlerApi from '../api/axiosInstance';
+import { DonationGiveListItem, DonationListRequest } from '../types/Donation';
+import { useContext } from 'react';
+import { AuthenticationContext } from '../contexts/AuthenticationContext';
+import { PageResponse } from '../types/Page';
+import dateFormat from '../utils/dateFormat';
+
+const useFetchDonationList = (request: DonationListRequest) => {
+  const { authentication } = useContext(AuthenticationContext);
+
+  return useQuery(
+    [
+      'donation give list',
+      authentication.id,
+      request.start,
+      request.end,
+      request.number,
+      request.size,
+    ],
+    async () => {
+      const response = await butlerApi.get<PageResponse<DonationGiveListItem>>(
+        '/api/donation/give',
+        {
+          headers: {
+            Authorization: `Bearer ${authentication.accessToken}`,
+          },
+          params: {
+            number: request.number,
+            size: request.size,
+            start: dateFormat(request.start, 'YYYY-MM-DDT00:00:00'),
+            end: dateFormat(request.end, 'YYYY-MM-DDT23:59:59.999'),
+          },
+        },
+      );
+      return response.data;
+    },
+    // {
+    //   enabled: false,
+    // },
+  );
+};
+
+export default useFetchDonationList;
---
+++ src/hooks/useFetchDonationReceiveList.tsx
@@ -0,0 +1,43 @@
+import { useQuery } from 'react-query';
+import butlerApi from '../api/axiosInstance';
+import {
+  DonationReceiveListItem,
+  DonationListRequest,
+} from '../types/Donation';
+import { useContext } from 'react';
+import { AuthenticationContext } from '../contexts/AuthenticationContext';
+import { PageResponse } from '../types/Page';
+import dateFormat from '../utils/dateFormat';
+
+const useFetchDonationReceiveList = (request: DonationListRequest) => {
+  const { authentication } = useContext(AuthenticationContext);
+
+  return useQuery(
+    [
+      'donation receive list',
+      authentication.id,
+      request.start,
+      request.end,
+      request.number,
+      request.size,
+    ],
+    async () => {
+      const response = await butlerApi.get<
+        PageResponse<DonationReceiveListItem>
+      >('/api/donation/receive', {
+        headers: {
+          Authorization: `Bearer ${authentication.accessToken}`,
+        },
+        params: {
+          number: request.number,
+          size: request.size,
+          start: dateFormat(request.start, 'YYYY-MM-DDT00:00:00'),
+          end: dateFormat(request.end, 'YYYY-MM-DDT23:59:59.999'),
+        },
+      });
+      return response.data;
+    },
+  );
+};
+
+export default useFetchDonationReceiveList;
