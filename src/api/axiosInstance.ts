9a35715a5355c604b237e1514e10f43157d82857
skatn <87296704+skatn@users.noreply.github.com>
2024-05-14 20:27:26 +0900

    #33 fix: axios accessToken 헤더 추가 interceptor 적용

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,9 +1,19 @@
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 import { API_HOST, USE_MOCK } from '../constants/api';
+import { getAuthenticationFromLocalStorage } from '../contexts/AuthenticationContext';
 
 const butlerApi = axios.create({
   baseURL: API_HOST,
+});
+
+butlerApi.interceptors.request.use((config) => {
+  const authentication = getAuthenticationFromLocalStorage();
+  if (authentication.isAuthenticated) {
+    console.log(authentication);
+    config.headers.Authorization = `Bearer ${authentication.accessToken}`;
+  }
+  return config;
 });
 
 export const mockAdapter = new MockAdapter(butlerApi);
