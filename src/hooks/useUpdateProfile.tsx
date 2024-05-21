b80809e6318272318ffc79dabe5ec6c8a7294266
skatn <87296704+skatn@users.noreply.github.com>
2024-05-21 20:22:08 +0900

    #48 fix: update profile useMutation 적용

--- src/hooks/useUpdateProfile.tsx
+++ src/hooks/useUpdateProfile.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import butlerApi from '../api/axiosInstance';
 import { MemberProfileUpdateRequest } from '../types/Member';
+import { useMutation, useQueryClient } from 'react-query';
 
 const useUpdateProfile = () => {
   const [param, setParam] = useState<MemberProfileUpdateRequest>({
@@ -10,18 +11,21 @@ const useUpdateProfile = () => {
     profileImage: new File([], 'empty_image.png', { type: 'image/png' }),
   });
 
-  const submit = async () => {
+  const mutation = useMutation((param: MemberProfileUpdateRequest) => {
     const formData = new FormData();
     formData.append('name', param.name);
     formData.append('introduction', param.introduction);
     formData.append('profileImage', param.profileImage);
 
-    const response = await butlerApi.post(`/api/member/${param.id}`, formData, {
+    return butlerApi.post(`/api/member/${param.id}`, formData, {
       headers: {
         'Content-Type': 'multipart/form-data',
       },
     });
+  });
 
+  const submit = async () => {
+    const response = await mutation.mutateAsync(param);
     return response.data;
   };
 
