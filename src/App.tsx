9cbad2390b5d48c82949fe70f11efa0adf72799c
skatn <87296704+skatn@users.noreply.github.com>
2024-05-22 20:47:58 +0900

    #50 fix: container component overflow-scroll 제거

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,7 @@ import TagPostPage from './pages/TagPostPage';
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
+      staleTime: 60000,
       retry: false,
     },
   },
--- src/pages/RootPage.tsx
+++ src/pages/RootPage.tsx
@@ -2,11 +2,23 @@ import { Outlet } from 'react-router-dom';
 import BottomNavigation from '../components/bottomNavigation/BottomNavigation';
 import AxiosConfig from '../api/AxiosConfig';
 
-const RootPage = () => {
+/*const RootPage = () => {
   return (
     <div className="flex h-dvh flex-col">
       <AxiosConfig />
       <div className="flex-grow overflow-y-scroll scrollbar-hide">
+        <Outlet />
+      </div>
+      <BottomNavigation />
+    </div>
+  );
+};*/
+
+const RootPage = () => {
+  return (
+    <div className="relative flex min-h-dvh flex-col bg-white">
+      <AxiosConfig />
+      <div className="flex-grow">
         <Outlet />
       </div>
       <BottomNavigation />
