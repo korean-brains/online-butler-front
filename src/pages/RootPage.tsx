55c5a393027dc18d727708123fb8f491c4ec3305
skatn <87296704+skatn@users.noreply.github.com>
2024-05-22 20:56:24 +0900

    #50 comment: 주석 삭제

--- src/pages/RootPage.tsx
+++ src/pages/RootPage.tsx
@@ -2,23 +2,11 @@ import { Outlet } from 'react-router-dom';
 import BottomNavigation from '../components/bottomNavigation/BottomNavigation';
 import AxiosConfig from '../api/AxiosConfig';
 
-/*const RootPage = () => {
-  return (
-    <div className="flex h-dvh flex-col">
-      <AxiosConfig />
-      <div className="flex-grow overflow-y-scroll scrollbar-hide">
-        <Outlet />
-      </div>
-      <BottomNavigation />
-    </div>
-  );
-};*/
-
 const RootPage = () => {
   return (
-    <div className="relative flex min-h-dvh flex-col bg-white">
+    <div className="relative min-h-dvh bg-white">
       <AxiosConfig />
-      <div className="flex-grow">
+      <div className="w-full">
         <Outlet />
       </div>
       <BottomNavigation />
